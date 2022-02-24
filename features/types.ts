7fca827873833f8f9b20f52185981b995f8bdde9
rabos23 <rabos23@gmail.com>
2022-02-24 15:21:44 +0100

    init

---
+++ features/types.ts
@@ -0,0 +1,32 @@
+const today = new Date();
+
+today.getMonth();
+
+const person = {
+  age: 20,
+  name: "name",
+};
+
+class Color {}
+
+const red = new Color();
+
+const add = (a: number, b: number) => a + b;
+
+const sub = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+const result = add(1, 2);
+console.log(result);
+console.log(add(1, 2));
+
+ 
